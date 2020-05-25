e871cfaf962103168e3c6dbbf5b3456dcce39f44
alfredocnt <alfredo.cnt@gmail.com>
2020-05-25 12:04:22 -0300

    adding files for github pages

---
+++ build/precache-manifest.bcb02ba986dc2a2717f5b2d2a2f8fa1e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1da7392edd5defb3d1d7f100c5e00c16",
+    "url": "/alfredocnt/monster_roladex/index.html"
+  },
+  {
+    "revision": "bbc421a164c739ca38c0",
+    "url": "/alfredocnt/monster_roladex/static/css/main.b1169781.chunk.css"
+  },
+  {
+    "revision": "9d36cb1448d0cd9bbf34",
+    "url": "/alfredocnt/monster_roladex/static/js/2.9d52131b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/alfredocnt/monster_roladex/static/js/2.9d52131b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bbc421a164c739ca38c0",
+    "url": "/alfredocnt/monster_roladex/static/js/main.506687eb.chunk.js"
+  },
+  {
+    "revision": "cec6d57ee45085ce50fe",
+    "url": "/alfredocnt/monster_roladex/static/js/runtime-main.9f77e48b.js"
+  }
+]);
